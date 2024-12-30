993ada7ce36f3bcd289c889fac07916c2885b01e
bidoluyazılım <163304841+hllsimsekk@users.noreply.github.com>
2024-12-30 18:17:54 +0300

    degisiklikler gerceklestirildi

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from veri_kaynagi import bilgileri_yukle
-import yaml
+
 
 def yas_hesaplama():
     print("Yas Hesaplama Uygulamasi")
@@ -16,35 +16,19 @@ def yas_hesaplama():
         with open("cikti.txt", "w") as cikti_dosya:
             for satir in satirlar:
                 ad, dogum_yili = satir.strip().split(",")  # Ad ve doğum yılı ayrıştırılır
-                yas = mevcut_yil - int(dogum_yili)
-                sonuc = f"{ad} su an {yas} yasinda."
-                print(sonuc)  # Konsola yazdır
-                cikti_dosya.write(sonuc + "\n")  # Dosyaya yazdır
+                dogum_yili = int(dogum_yili)
+                if dogum_yili > mevcut_yil:
+                    sonuc = f"{ad} icin gecersiz dogum yili: {dogum_yili}. Yas hesaplanamadi."
+                    print(sonuc)  # Geçersiz yaş mesajını konsola yazdır
+                    cikti_dosya.write(sonuc + "\n")  # Geçersiz yaş mesajını dosyaya yazdır
+                else:
+                    yas = mevcut_yil - dogum_yili
+                    sonuc = f"{ad} su an {yas} yasinda."
+                    print(sonuc)  # Konsola yazdır
+                    cikti_dosya.write(sonuc + "\n")  # Dosyaya yazdır
     except Exception as e:
         print(f"Bir hata olustu: {e}")
 
-def yaml_dosyasina_yaz():
-    # cikti.txt dosyasını açıp içeriğini oku
-    try:
-        with open('cikti.txt', 'r') as file:
-            cikti_verileri = file.readlines()
 
-        # Veriyi işleyip bir listeye dönüştürme
-        cikti_listesi = [satir.strip() for satir in cikti_verileri]
-
-        # YAML dosyasına yazma işlemi
-        yaml_veri = {'cikti': cikti_listesi}
-
-        # YAML dosyasına yazma
-        with open('output.yml', 'w') as yaml_file:
-            yaml.dump(yaml_veri, yaml_file, default_flow_style=False)
-
-        print("YAML dosyasına yazıldı.")
-    except Exception as e:
-        print(f"YAML yazma hatası: {e}")
-
-# Yas hesaplama işlemi
+# Uygulamayı çalıştır
 yas_hesaplama()
-
-# YAML dosyasına yazma işlemi
-yaml_dosyasina_yaz()
